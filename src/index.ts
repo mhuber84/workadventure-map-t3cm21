7b7f1e522baffb293a772bb5faa7ab92201f69ad
mhuber84 <mhuber84@users.noreply.github.com>
2021-08-31 16:34:36 +0000

    Deploying to gh-pages from master @ b6ee9c5b812d7dc8c5b2059549987c535d72877f 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,67 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
 let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
 
-WA.room.onEnterZone('clock', () => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
+WA.room.onEnterZone('twitter', () => {
+    currentPopup = WA.openPopup(
+        'followUsPopup',
+        'Hey! Want to follow us on Twitter?',
+        [
+          {
+              label: 'Twitter',
+              className: 'primary',
+              callback: () => WA.nav.openTab('https://twitter.com/t3cm'),
+          }
+      ]
+    )
 })
+WA.room.onLeaveZone('twitter', closePopUp)
 
-WA.room.onLeaveZone('clock', closePopUp)
+WA.room.onEnterZone('instagram', () => {
+    currentPopup = WA.openPopup(
+        'followUsPopup',
+        'Hey! Want to follow us on Instagram?',
+        [
+          {
+              label: 'Instagram',
+              className: 'primary',
+              callback: () => WA.nav.openTab('https://www.instagram.com/typo3camp/'),
+          }
+      ]
+    )
+})
+WA.room.onLeaveZone('instagram', closePopUp)
+
+WA.room.onEnterZone('facebook', () => {
+    currentPopup = WA.openPopup(
+        'followUsPopup',
+        'Hey! Want to follow us on Facebook?',
+        [
+          {
+              label: 'Facebook',
+              className: 'primary',
+              callback: () => WA.nav.openTab('https://www.facebook.com/TYPO3campMunich'),
+          }
+      ]
+    )
+})
+WA.room.onLeaveZone('facebook', closePopUp)
+
+WA.room.onEnterZone('slack', () => {
+    currentPopup = WA.openPopup(
+        'followUsPopup',
+        'Hey! Want to follow us on Slack?',
+        [
+          {
+              label: 'Slack',
+              className: 'primary',
+              callback: () => WA.nav.openTab('https://typo3.slack.com/archives/CC4DW9479'),
+          }
+      ]
+    )
+})
+WA.room.onLeaveZone('slack', closePopUp)
+
 
 function closePopUp(){
     if (currentPopup !== undefined) {
